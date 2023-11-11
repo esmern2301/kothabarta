8b7feadb3cf601a8e452fb316e65f7734378bc0f
esmern2301 <citesmern2301@gmail.com>
2023-11-11 19:37:50 +0600

    d-11 [block]

--- src/components/BlockList/BlockList.jsx
+++ src/components/BlockList/BlockList.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import profile from '../../assets/profile.png'
 import {BiDotsVerticalRounded} from 'react-icons/bi'
+import { getDatabase, ref, onValue, set, push, remove } from "firebase/database";
+import { useSelector } from 'react-redux';
 
 const BlockList = () => {
+    const data = useSelector(state => state.user.userInfo);
+    const db = getDatabase();
+
+    const [blockList, setBlockList] = useState([]);
+
+    useEffect(() => {
+        const blockRef = ref(db, 'block/');
+        onValue(blockRef, (snapshot) => {
+            let arr = []
+            snapshot.forEach((item) => {
+               console.log(item.val(), 'block');
+                if(item.val().blockbyid == data.uid){
+                    arr.push({
+                        id: item.key,
+                        block: item.val().block,
+                        blockid: item.val().blockid
+                    })
+                }else{
+                    arr.push({
+                        id: item.key,
+                        blockby: item.val().blockby,
+                        blockbyid: item.val().blockbyid
+                    })
+                }
+             
+            })
+            setBlockList(arr)
+        });
+    }, [])
   return (
     <div className=" bg-white border rounded-[20px] px-[23px] py-[20px] drop-shadow-lg mb-4 ">
         <div className='flex items-center justify-between'>
@@ -11,78 +42,30 @@ const BlockList = () => {
         </div>
    
         <div className='mt-[20px] h-[400px] overflow-y-scroll'>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
-            </div>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
-            </div>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
-            </div>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
-            </div>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
-            </div>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
-            </div>
+            {
+                blockList.map((item)=>(
+                    <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
+                        <div>
+                            <img src={profile} className="rounded-full" />
+                        </div>
+                        <div>
+                            <h3 className="text-xl font-bold text-[20px] font-pops">{item.block}</h3>
+                            <h3 className="text-xl font-bold text-[20px] font-pops">{item.blockby}</h3>
+                            
+
+                            <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
+                        </div>
+                        <div>
+                            {
+                             !item.blockbyid &&
+                             <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>UnBlock</button>
+                            }
+                            
+                        </div>
+                    </div>
+                ))
+            }
+            
         </div>
 
 </div>
--- src/components/Friends/Friends.jsx
+++ src/components/Friends/Friends.jsx
@@ -1,92 +1,87 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import profile from '../../assets/profile.png'
-import {BiDotsVerticalRounded} from 'react-icons/bi'
+import { BiDotsVerticalRounded } from 'react-icons/bi'
+import { getDatabase, ref, onValue, set, push, remove } from "firebase/database";
+import { useSelector } from 'react-redux';
+
 const Friends = () => {
-  return (
-    <div className=" bg-white border rounded-[20px] px-[23px] py-[20px] drop-shadow-lg mb-4 ">
-        <div className='flex items-center justify-between'>
-            <h3 className='text-[24px] font-pops font-semibold'>Friends</h3>
-            <BiDotsVerticalRounded className="text-2xl text-primary" />
-        </div>
-   
-        <div className='mt-[20px] h-[400px] overflow-y-scroll'>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
+    const data = useSelector(state => state.user.userInfo);
+    const db = getDatabase();
+
+    const [friendList, setFriendList] = useState([])
+    useEffect(() => {
+        const friendRef = ref(db, 'friend/');
+        onValue(friendRef, (snapshot) => {
+            let arr = []
+            snapshot.forEach((item) => {
+                //    console.log(item.val(), 'frlist');
+                if(data.uid == item.val().receiverid || data.uid == item.val().senderid){
+                    arr.push({...item.val(), key:item.key})
+                }
+
+            })
+            setFriendList(arr)
+        });
+    }, [])
+
+
+    const handleBlock = (item) =>{
+        console.log(item);
+        if(data.uid == item.senderid){
+            set(push(ref(db, 'block/')), {
+                block: item.receivername,
+                blockid: item.receiverid,
+                blockby: item.sendername,
+                blockbyid: item.senderid
+             }).then(()=>{
+                remove(ref(db, 'friend/' + item.key))
+             })
+        }else{
+            set(push(ref(db, 'block/')), {
+                block: item.sendername,
+                blockid: item.senderid,
+                blockby: item.receivername,
+                blockbyid: item.receiverid
+             }).then(()=>{
+                remove(ref(db, 'friend/' + item.key))
+             })
+        }
+    }
+    return (
+        <div className=" bg-white border rounded-[20px] px-[23px] py-[20px] drop-shadow-lg mb-4 ">
+            <div className='flex items-center justify-between'>
+                <h3 className='text-[24px] font-pops font-semibold'>Friends</h3>
+                <BiDotsVerticalRounded className="text-2xl text-primary" />
             </div>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
+
+            <div className='mt-[20px] h-[400px] overflow-y-scroll'>
+                {
+                    friendList.map((item) => (
+                        <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
+                            <div>
+                                <img src={profile} className="rounded-full" />
+                            </div>
+                            <div>
+                                <h3 className="text-xl font-bold text-[20px] font-pops">
+                                    {
+                                        data.uid == item.senderid ? item.receivername : item.sendername
+                                    }
+                                </h3>
+                                <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
+                            </div>
+                            <div>
+                                <button onClick={()=>handleBlock(item)} className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Block</button>
+                            </div>
+                        </div>
+                    ))
+                }
+
+
             </div>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
-            </div>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
-            </div>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
-            </div>
-            <div className="flex items-center justify-between border-b-gray-300 border border-x-0 border-t-0 last:border-none py-[10px] mb-[10px]">
-                <div>
-                    <img src={profile} className="rounded-full" />
-                </div>
-                <div>
-                    <h3 className="text-xl font-bold text-[20px] font-pops">Friends Reunion</h3>
-                    <p className="text-gray-500 font-bold text-[14px] font-pops">Hi Guys, Wassup!</p>
-                </div>
-                <div>
-                    <button className='bg-primary text-white px-[22px] font-pops rounded text-[20px]'>Join</button>
-                </div>
-            </div>
+
         </div>
 
-</div>
-    
-  )
+    )
 }
 
 export default Friends
